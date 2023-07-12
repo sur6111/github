2b58dfcd38740bd3cef6319c333d41de2250ec5c
Sahil Sonkar <sonkarsahil04@gmail.com>
2023-07-12 21:11:50 +0530

    added docker base files and scripts

---
+++ network/scripts/installChaincode.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Deploying Chaincode MEDTRACNET On medtrac Network"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+VERSION="$4"
+TYPE="$5"
+: ${CHANNEL_NAME:="medtracchannel"}
+: ${DELAY:="5"}
+: ${LANGUAGE:="node"}
+: ${VERSION:=1.1}
+: ${TYPE="basic"}
+
+LANGUAGE=$(echo "$LANGUAGE" | tr [:upper:] [:lower:])
+ORGS="manufacturer distributor transporter retailer consumer"
+TIMEOUT=15
+
+if [ "$TYPE" = "basic" ]; then
+    CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric/peer/chaincode/"
+else
+    CC_SRC_PATH="/opt/gopath/src/github.com/hyperledger/fabric/peer/chaincode-advanced/"
+fi
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+## Install new version of chaincode on both peer0 & peer1 of all 5 orgs
+echo "Installing chaincode on peer0.manufacturer.medtrac-network.com ..."
+installChaincode 0 'manufacturer' $VERSION
+echo "Installing chaincode on peer1.manufacturer.medtrac-network.com ..."
+installChaincode 1 'manufacturer' $VERSION
+echo "Installing chaincode on peer0.distributor.medtrac-network.com ..."
+installChaincode 0 'distributor' $VERSION
+echo "Installing chaincode on peer1.distributor.medtrac-network.com ..."
+installChaincode 1 'distributor' $VERSION
+echo "Installing chaincode on peer0.transporter.medtrac-network.com ..."
+installChaincode 0 'transporter' $VERSION
+echo "Installing chaincode on peer1.transporter.medtrac-network.com ..."
+installChaincode 1 'transporter' $VERSION
+echo "Installing chaincode on peer0.retailer.medtrac-network.com ..."
+installChaincode 0 'retailer' $VERSION
+echo "Installing chaincode on peer1.retailer.medtrac-network.com ..."
+installChaincode 1 'retailer' $VERSION
+echo "Installing chaincode on peer0.consumer.medtrac-network.com ..."
+installChaincode 0 'consumer' $VERSION
+echo "Installing chaincode on peer1.consumer.medtrac-network.com ..."
+installChaincode 1 'consumer' $VERSION
+
+# Instantiate chaincode on the channel using peer0.manufacturer
+echo "Instantiating chaincode on channel using peer0.manufacturer.medtrac-network.com ..."
+instantiateChaincode 0 'manufacturer' $VERSION
+
+echo
+echo "========= All GOOD, Chaincode MEDTRACNET Is Now Installed & Instantiated On Medtrac Network =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
